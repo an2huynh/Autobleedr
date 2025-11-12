cf40fa66fff2af0d4fbd8b09c1eff0e1654cf3c1
An Huynh <an2huynh@gmail.com>
2025-11-11 20:28:58 -0500

    integrated debubbler

--- main/debubble.cpp
+++ main/debubble.cpp
@@ -3,13 +3,24 @@
 #include <stdint.h>
 #include <Arduino.h>
 
+const int pwm_pin = 26;
+const int dir_pin = 27;
+const int enable_pin = 14;
+
+void InitializeDebubblerPins() {
+  analogWriteResolution(pwm_pin, 8);     // duty range 0..255
+  analogWriteFrequency(pwm_pin, 1000);   // 1 kHz PWM
+  analogWrite(pwm_pin, 127);        // ~5% duty on pwm_pin
+  pinMode(dir_pin, OUTPUT);
+  pinMode(enable_pin, OUTPUT);
+  digitalWrite(dir_pin, HIGH);
+  digitalWrite(enable_pin, HIGH);
+}
+
 bool DeBubbler(void) {
-  for (int i = 0; i < 4; ++i) {
-    digitalWrite(SYR_TEST_LED_PIN , HIGH);
-    delay(250);                 // on for 200 ms
-    digitalWrite(SYR_TEST_LED_PIN , LOW);
-    delay(250);                 // off for 200 ms
-  }
+  analogWrite(pwm_pin, 160); delay(3000);
+  analogWrite(pwm_pin, 0);
+}
   
   return true;
 }
